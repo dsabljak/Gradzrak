d969e83b4629b5bbf9d632989f72b031c7410900
dsabljak98 <dsabljak98@gmail.com>
2019-08-31 17:25:16 +0200

    Adding to gitignore

--- timeproba.py
+++
@@ -1,9 +0,0 @@
-import datetime
-import re
-
-
-TimestampUtc = "\/Date(1566958055158)\/"
-
-TimestampUtc = re.split('\(|\)', TimestampUtc)[1][:10]
-date = datetime.datetime.fromtimestamp(int(TimestampUtc))
-print(date)
