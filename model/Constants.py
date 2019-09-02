e8afc660926867f5a1b084a013085cb5c4642d2d
dsabljak98 <dsabljak98@gmail.com>
2019-09-02 19:36:40 +0200

    Added constants to specific file. Fixed loaction check (didn't work for negative coords)

---
+++ model/Constants.py
@@ -0,0 +1,26 @@
+
+
+class Constants():
+
+    url1    = "https://s5phub.copernicus.eu/dhus/odata/v1/Products"
+    url2    = "https://scihub.copernicus.eu/apihub/odata/v1/Products"
+
+    s5pLogin = ("s5pguest", "s5pguest")
+    s123Login = ("dsabljak", "Jabuka!=42")
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    gasList = ["CO2", "CO"]
+
+    s1productList = ["SLC", "GRD", "OCN"]
+
+    s2productList = ["S2MSI1C", "S2MSI2A", "S2MSI2Ap"]
+
+    s3productList = ["OL_1_EFR___", "OL_1_ERR___", "OL_2_LFR___", "OL_2_WFR___", "OL_2_LRR___", "SR_1_SRA___", "SR_1_SRA_A_", "SR_1_SRA_BS", "SR_2_LAN___"
+                     "SL_1_RBT___", "SL_2_LST___", "SY_2_SYN___", "SY_2_V10___", "SY_2_VG1___", "SY_2_VGP___"]
+
+    s5pproductList = ["L1B_IR_SIR", "L1B_IR_UVN", "L1B_RA_BD1", "L1B_RA_BD2", "L1B_RA_BD3", "L1B_RA_BD4", "L1B_RA_BD5", "L1B_RA_BD6", "L1B_RA_BD7",
+                      "L1B_RA_BD8", "L2__AER_AI", "L2__CH4___", "L2__CLOUD_", "L2__CO____", "L2__HCHO__", "L2__NO2___", "L2__NP_BD3", "L2__NP_BD6",
+                      "L2__NP_BD7", "L2__O3_TCL", "L2__O3____", "L2__SO2___"]
+
+    townList = ["Zagreb", "Split", "Osijek", "Rijeka"]
+
+    townCoords = {"Zagreb": [45.815399, 15.966568], "Split": [43.508133, 16.440193], "Osijek": [45.5464306, 18.6188185], "Rijeka": [45.3477092, 14.3567736]}
